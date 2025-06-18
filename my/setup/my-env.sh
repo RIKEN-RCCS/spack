a95659366ef0c70f6ed7a8b1efd3bbc6df3d5aff
Yuichi Otsuka <otsukay@riken.jp>
2025-06-18 14:31:52 +0900

    Migrate builtin and local repogitories

---
+++ my/setup/my-env.sh
@@ -0,0 +1,4 @@
+# do not share cache with other spack instance
+#export SPACK_USER_CONFIG_PATH=${SPACK_ROOT}/tmp/user_config
+export SPACK_DISABLE_LOCAL_CONFIG=true
+export SPACK_USER_CACHE_PATH=${SPACK_ROOT}/tmp/user_cache
